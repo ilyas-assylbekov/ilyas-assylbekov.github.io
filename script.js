acd0be6d3dba6663f30c272a2ecbbfa324f652c2
illyForeverEarly <ilyas.asylbekov1305@gmail.com>
2024-01-03 22:18:50 +0600

    Add minor changes

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-
+document.addEventListener('DOMContentLoaded', function () {
+    const showSocials = document.getElementById('show-socials');
+    const socialsMenu = document.getElementById('socials-menu');
+  
+    showSocials.addEventListener('click', function () {
+      socialsMenu.style.left = socialsMenu.style.left === '0px' ? '-120%' : '0px';
+      showSocials.textContent = socialsMenu.style.left === '0px' ? "Hide Socials" : "Show Socials";
+    });
+  });
   
